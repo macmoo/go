ce1d6125e20be79d78cee7cc585e09138f1a147b
kaku <hscrollhooli@gmail.com>
2023-03-26 18:51:34 +0900

    20230325

--- root2303/20230325.go
+++ root2303/20230325.go
@@ -1,24 +1,2 @@
 package main
-
-import "fmt"
-
-type Stringer interface {
-	String() string
-}
-
-type Student struct {
-	Name string
-	Age  int
-}
-
-func (s Student) String() string {
-	return fmt.Printf("HELLO, %d살, %s라고 한다.", s.Age, s.Name)
-}
-
-func main() {
-	fmt.Println("---------------------------------")
-	student := Student{"철수", 12}
-	var stringer Stringer
-	stringer = student
-	fmt.Printf("%s\n", stringer.String())
-}
+fmt.Println("---------------------------------")
